1bb6c05281f03d03fee6a95447d296c7114b06db
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-05-30 17:57:05 +0300

    add generating esm module format

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => ({
+  name: "reaviz-data-utils",
+  entry: ['src/index.tsx'],
+  format: ["esm", "cjs"],
+  bundle: true,
+  clean: true,
+}))
